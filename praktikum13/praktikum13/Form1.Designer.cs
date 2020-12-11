367fac44e8ed102a89799fd14c17dece2cc61f81
Bridged Livia <bgunawan03@student.ciputra.ac.id>
2020-12-11 09:03:21 +0700

    PraktikumWeek13

---
+++ praktikum13/praktikum13/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace praktikum13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelinputdata = new System.Windows.Forms.Label();
+            this.buttonproses = new System.Windows.Forms.Button();
+            this.textBoxpesan = new System.Windows.Forms.TextBox();
+            this.labelpesan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelinputdata
+            // 
+            this.labelinputdata.AutoSize = true;
+            this.labelinputdata.Location = new System.Drawing.Point(12, 9);
+            this.labelinputdata.Name = "labelinputdata";
+            this.labelinputdata.Size = new System.Drawing.Size(93, 20);
+            this.labelinputdata.TabIndex = 0;
+            this.labelinputdata.Text = "Input Data: ";
+            this.labelinputdata.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonproses
+            // 
+            this.buttonproses.Location = new System.Drawing.Point(12, 88);
+            this.buttonproses.Name = "buttonproses";
+            this.buttonproses.Size = new System.Drawing.Size(90, 35);
+            this.buttonproses.TabIndex = 1;
+            this.buttonproses.Text = "proses";
+            this.buttonproses.UseVisualStyleBackColor = true;
+            this.buttonproses.Click += new System.EventHandler(this.buttonproses_Click);
+            // 
+            // textBoxpesan
+            // 
+            this.textBoxpesan.Location = new System.Drawing.Point(12, 41);
+            this.textBoxpesan.Name = "textBoxpesan";
+            this.textBoxpesan.Size = new System.Drawing.Size(306, 26);
+            this.textBoxpesan.TabIndex = 2;
+            // 
+            // labelpesan
+            // 
+            this.labelpesan.AutoSize = true;
+            this.labelpesan.Location = new System.Drawing.Point(12, 155);
+            this.labelpesan.Name = "labelpesan";
+            this.labelpesan.Size = new System.Drawing.Size(71, 20);
+            this.labelpesan.TabIndex = 3;
+            this.labelpesan.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelpesan);
+            this.Controls.Add(this.textBoxpesan);
+            this.Controls.Add(this.buttonproses);
+            this.Controls.Add(this.labelinputdata);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelinputdata;
+        private System.Windows.Forms.Button buttonproses;
+        private System.Windows.Forms.TextBox textBoxpesan;
+        private System.Windows.Forms.Label labelpesan;
+    }
+}
+
---
+++ praktikum13/praktikum13/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace praktikum13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonproses_Click(object sender, EventArgs e)
+        {
+            if (labelpesan.Text == "[EMPTY]")
+            {
+                labelpesan.Font = new Font("Arial", 10, FontStyle.Regular);
+                labelpesan.ForeColor = Color.Black;
+                labelpesan.Text = textBoxpesan.Text;
+            }
+
+            if(textBoxpesan.Text=="DELETE" && labelpesan.Text!="[EMPTY]")
+            { labelpesan.Text = "[EMPTY]";
+                labelpesan.ForeColor = Color.Black;
+            }
+
+            if(textBoxpesan.Text=="HIDE" && labelpesan.Text!="[EMPTY]")
+            { labelpesan.Visible = false; }
+
+            if (textBoxpesan.Text == "SHOWN" && labelpesan.Text != "[EMPTY]")
+            { labelpesan.Visible = true; }
+
+            if(textBoxpesan.Text=="BLUE" && labelpesan.Text!="[EMPTY]")
+            { labelpesan.ForeColor = Color.Blue; }
+
+            if (textBoxpesan.Text == "RED" && labelpesan.Text != "[EMPTY]")
+            { labelpesan.ForeColor = Color.Red; }
+
+            if (textBoxpesan.Text == "GREEN" && labelpesan.Text != "[EMPTY]")
+            { labelpesan.ForeColor =Color.Green; }
+
+            
+
+        }
+    }
+}
